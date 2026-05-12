3a9ff0ad54e01a826ab70f7d7bf9803bb025170a
Sulav-Koirala <koiralasulav0@gmail.com>
2026-05-12 19:41:13 +0545

    update:task 4

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
-from app.routers import count_routers,customer_routers
+from app.routers import count_routers,customer_routers,employee_routers,office_routers,order_routers,\
+    payment_routers,product_routers,productline_routers,orderdetail_routers
 
 app = FastAPI()
-app.include_router(customer_routers.router)
+
 app.include_router(count_routers.router)
+
+app.include_router(customer_routers.router)
+app.include_router(employee_routers.router)
+app.include_router(office_routers.router)
+app.include_router(order_routers.router)
+app.include_router(payment_routers.router)
+app.include_router(product_routers.router)
+app.include_router(productline_routers.router)
+app.include_router(orderdetail_routers.router)
